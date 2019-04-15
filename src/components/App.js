2987a8cb0c74f597e8d71061816fac30572356d2
Ahmed Raza <raza1778@gmail.com>
2019-04-15 12:32:07 +0500

    Added loader

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,21 @@ import SearchBar from './SearchBar'
 import ImageList from './ImageList'
 
 class App extends React.Component {
-  state = { images: [] }
+  state = { images: [], load: 'none' }
   onSearchSubmit = async term => {
+    this.setState({ load: 'block' })
     const response = await unsplash.get('/search/photos', {
       params: {query: term}
     })
-    this.setState({ images: response.data.results })
+    this.setState({ images: response.data.results, load: 'none' })
   }
   render() {
     return (
         <div className="ui container" style={{ marginTop: '10px' }}>
           <SearchBar onSubmit={this.onSearchSubmit} />
+          <div style={{ display : this.state.load }} className="ui active dimmer">
+            <div className="ui loader"></div>
+          </div>
           <ImageList images={this.state.images} />
         </div>
       )
