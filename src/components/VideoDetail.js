b77ba2199b310c1a5ede467d5a5af6ff00dc7d78
Ahmed Raza <raza1778@gmail.com>
2019-04-18 13:23:16 +0500

    Formating date

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,5 +1,22 @@
 import React from 'react'
 
+const formatDate = (date) => {
+  let months = [
+    'January',
+    'Feburary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'November',
+    'December'
+  ];
+  let timestamp = new Date(date)
+  return months[timestamp.getMonth()] +' '+ timestamp.getDate() +', '+ timestamp.getFullYear()
+}
 const VideoDetail = ({ video }) => {
   if(!video) {
     return <div>Loading...</div>
@@ -13,7 +30,8 @@ const VideoDetail = ({ video }) => {
         <iframe title="video player" src={videoSrc} frameBorder="0"></iframe>
       </div>
       <div className="ui segment">
-        <h4 className="ui header">{video.snippet.title}</h4> <span style={{ float: 'right' }}>{video.snippet.publishedAt}</span>
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <em>{video.snippet.channelTitle}</em> <strong>{formatDate(video.snippet.publishedAt)}</strong>
         <p>{video.snippet.description}</p>
       </div>
     </div>
