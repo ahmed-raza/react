068dba1e9f57d051379e0f920d0e1db1de85d8c2
Alex Roye <alex@creativefaze.com>
2021-10-23 14:49:22 +0500

    Fresh setup

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React from "react";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
-
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Car Insurance",
-    price: "294.67",
-    date: new Date(2021, 9, 13),
-  },
-  {
-    id: "e2",
-    title: "Doctor Fee",
-    price: "312.67",
-    date: new Date(2021, 5, 8),
-  },
-  {
-    id: "e3",
-    title: "Grocery",
-    price: "321.67",
-    date: new Date(2020, 8, 24),
-  },
-];
+import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { expenses: DUMMY_EXPENSES };
-    this.addExpenseHandler = this.addExpenseHandler.bind(this);
-  }
-
-  addExpenseHandler(expense) {
-    this.setState({ expenses: [expense, ...this.state.expenses] });
-  }
-
   render() {
-    return (
-      <div className="App">
-        <NewExpense onAddExpense={this.addExpenseHandler} />
-        <Expenses items={this.state.expenses} />
-      </div>
-    );
+    return <div>Hello World!</div>
   }
 }
 
-export default App;
+export default App
