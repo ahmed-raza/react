0c266744a4e0e8557d4057d438221c3264550f35
Alex Roye <alex@creativefaze.com>
2021-10-27 21:07:48 +0500

    Empty app component to start with

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-// Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-// Create a React component
-
-function getTime() {
-  return (new Date()).toLocaleTimeString()
-}
-
-const App = () => {
-  return (
-    <div>
-      <div>Current Time:</div>
-      <h3>{ getTime() }</h3>
-    </div>
-  )
-}
-
-// Take the react component and show it on screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
