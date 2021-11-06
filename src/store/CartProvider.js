a7588488ea263d2ab883aab8423a53d367b1a638
Alex Roye <alex@creativefaze.com>
2021-11-06 17:35:32 +0500

    use of reducers

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCart = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItem = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCart = (item) => {
+    dispatchCartAction({
+      type: "ADD_ITEM",
+      item: item,
+    });
+  };
+
+  const removeItem = (id) => {
+    dispatchCartAction({
+      type: "REMOVE_ITEM",
+      id: id,
+    });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCart,
     removeItem: removeItem,
   };
