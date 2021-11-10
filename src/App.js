2bc9952de194a4812a9fdf213e374243694b0751
Alex Roye <alex@creativefaze.com>
2021-11-10 22:42:03 +0500

    Movies list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "semantic-ui-css/semantic.min.css";
+
+import { Container, Card, Button, Dimmer, Loader } from "semantic-ui-react";
 
 function App() {
-  return <div>App</div>;
+  const [movies, setMovies] = useState([]);
+  const [load, setLoad] = useState(false);
+  function fetchMovies() {
+    setLoad(true);
+    fetch("https://swapi.dev/api/films")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movie) => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            openingText: movie.opening_crawl,
+            releaseDate: movie.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+        setLoad(false);
+      });
+  }
+
+  return (
+    <Container>
+      <Button
+        style={{ marginTop: "7px" }}
+        className="primary"
+        onClick={fetchMovies}
+      >
+        Fetch Movies
+      </Button>
+      <hr />
+      <Card className="fluid">
+        <Dimmer active={load}>
+          <Loader />
+        </Dimmer>
+        <Card.Content>
+          <Card.Header>Movies</Card.Header>
+          <Card.Content>
+            <MoviesList movies={movies} />
+          </Card.Content>
+        </Card.Content>
+      </Card>
+    </Container>
+  );
 }
 
 export default App;
