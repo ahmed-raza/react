70838aaea17bfd19924980f0a2fc0d1527c2e419
Alex Roye <alex@creativefaze.com>
2023-03-15 23:55:42 +0500

    Removed getAuthUser function

--- src/components/util/auth.js
+++ src/components/util/auth.js
@@ -1,5 +1,4 @@
 import { redirect } from "react-router-dom";
-import axios from "axios";
 
 export function getAuthToken() {
   const token = localStorage.getItem("access_token");
@@ -8,29 +7,6 @@ export function getAuthToken() {
 
 export function tokenLoader() {
   return getAuthToken();
-}
-
-export function getAuthUser() {
-  const token = getAuthToken();
-  const user = axios
-    .get("http://127.0.0.1:8000/api/user", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "applicaton/json",
-        Authorization: "Bearer " + token,
-      },
-    })
-    .then((res) => {
-      return res;
-    })
-    .catch((res) => {
-      return res;
-    });
-  console.log(user);
-  if (user.data === 200) {
-    return user.data;
-  }
-  return user;
 }
 
 export function checkAuthLoader() {
