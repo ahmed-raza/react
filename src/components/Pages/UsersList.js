48234efe4d8b7569183f931f3fa94c381f1a8215
Alex Roye <alex@creativefaze.com>
2023-03-17 01:30:16 +0500

    User Delete functionality

--- src/components/Pages/UsersList.js
+++ src/components/Pages/UsersList.js
@@ -2,17 +2,21 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "../UI/Card";
+import Messages from "../UI/Messages";
 import { getAuthToken } from "../util/auth";
 
 const UsersList = () => {
   const [users, setUsers] = useState();
+  const [user, setUser] = useState();
+  const [messages, setMessages] = useState();
+  const token = getAuthToken();
 
   useEffect(() => {
+    getAuthUser();
     getUsersHandler();
   }, []);
 
   async function getUsersHandler() {
-    const token = getAuthToken();
     await axios
       .get("http://127.0.0.1:8000/api/users", {
         headers: {
@@ -26,8 +30,45 @@ const UsersList = () => {
       });
   }
 
+  async function getAuthUser() {
+    await axios
+      .get("http://127.0.0.1:8000/api/user", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        setUser(response.data);
+      });
+  }
+
+  const deleteUser = (id) => {
+    if (id == user.id) {
+      console.log(user);
+      return;
+    }
+    axios
+      .get("http://127.0.0.1:8000/api/delete-user/" + id, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        setMessages(response.data);
+        getUsersHandler();
+      })
+      .catch((response) => {
+        console.log(response);
+      });
+  };
+
   return (
     <Card title="Users">
+      {messages && <Messages messages={messages} />}
       <Link to="/new-user">Add User</Link>
       <hr />
       <table border="1" width="100%">
@@ -53,7 +94,8 @@ const UsersList = () => {
                 <td>{user.role}</td>
                 <td>{user.created_at}</td>
                 <td>
-                  <Link to={`/user/${user.id}/edit`}>Edit</Link> | Delete
+                  <Link to={`/user/${user.id}/edit`}>Edit</Link> |{" "}
+                  <button onClick={() => deleteUser(user.id)}>Delete</button>
                 </td>
               </tr>
             ))}
